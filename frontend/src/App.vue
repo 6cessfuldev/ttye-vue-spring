8425ff40ce6dfabacbdcabb5575f4f7156ae4d7e
6cessfuldev <6cessfuldev@gmail.com>
2023-06-03 23:44:05 +0900

    Update HeaderNav

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <HeaderNav></HeaderNav>
+  <HeaderNav v-if="loginStatus"></HeaderNav>
   <transition name="slide-fade">
       <router-view :key="$route.fullPath"/>
   </transition>
@@ -8,10 +8,35 @@
 </template>
 <script>
 import HeaderNav from '@/components/HeaderNav.vue'
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
 export default {
   name: 'App',
   components: {
     HeaderNav
+  },
+  setup() {
+
+    const loginStatus = ref(false)
+
+    onMounted(() => {
+      axios.get("http://localhost:8080/user/isLoggedIn")
+      .then(function(response){
+        if(!response.data) {
+          loginStatus.value = false;
+        } else {
+          loginStatus.value = true;
+        }
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+    })
+
+    return {
+      loginStatus
+    }
   }
 }
 
